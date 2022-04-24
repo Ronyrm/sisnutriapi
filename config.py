4ca2e8a58df625e25f8d67789fd7cd4c4a998a54
Rony Magalhaes <ronygustavo_rm@yahoo.com.br>
2022-04-24 10:31:55 -0300

    rony always

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 random_str = string.ascii_letters + string.digits + string.ascii_uppercase
 key = ''.join(random.choice(random_str) for i in range(12))
 DEBUG = True
-#SQLALCHEMY_DATABASE_URI = 'mysql://221068:rony0608@mysql-rgmsolutions.alwaysdata.net/rgmsolutions_siscontrol'
-SQLALCHEMY_DATABASE_URI = 'mysql://rony:rony@localhost:3307/sisnutri'
+SQLALCHEMY_DATABASE_URI = 'mysql://221068:rony0608@mysql-rgmsolutions.alwaysdata.net/rgmsolutions_siscontrol'
+#SQLALCHEMY_DATABASE_URI = 'mysql://rony:rony@localhost:3307/sisnutri'
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:root@db/sisnutri'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
